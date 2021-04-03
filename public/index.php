0f11e762e9011e16c1d7d98dc55faea916dcf9d8
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-03 11:24:57 +0200

    base

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./styles/css/build/bootstrap-launcher.min.css">
+    <link rel="stylesheet" href="./icons/vendors/bootstrap-icons/bootstrap-icons.css">
+    <!-- <link rel="stylesheet" href="./styles/css/constructor.css"> -->
+</head>
+
+<body>
+<header class="container-fluid">
+    <div class="row">
+        <div class="col-8 mt-7">
+                    <a href="">dsflmjkldsjdsjl</a>                                                            <p class="text-primary">hello world</p>
+        </div>
+        <div class="col-4"></div>
+    </div>
+</header>
+    <?php include_once "./layout/header.php" ?>
+    <div class="bg-rouge" style="width: 300px; height: 300px;"></div>
+    <?php include_once "./layout/footer.php" ?>
+    <script src="./js/dist/jquery/jquery-3.6.0.slim.min.js"></script>
+    <script src="./js/dist/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ public/layout/header.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="navbar navbar-expand-lg navbar-light bg-danger">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <nav class="navbar-nav navbar-collapse collapse justify-content-center" id="navbarSupportedContent">
+      <a class="nav-link active " href="#">Home</a>
+      <a class="nav-link " href="#">Link</a>
+    </nav>
+  </div>
+</header>
